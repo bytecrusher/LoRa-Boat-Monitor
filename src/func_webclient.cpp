0627e5f97d7295a891cceb7955315105af45a378
bytecrusher <guntmar.h@gmx.net>
2024-06-13 23:06:00 +0200

    changed the way, how webclient will download the web files from the server.

--- src/func_webclient.cpp
+++ src/func_webclient.cpp
@@ -13,172 +13,167 @@
 
 void DownloadFilesFromWeb(char *fversion)
 {
-    DownloadFile("css_black.css", fversion);
-    DownloadFile("css_red.css", fversion);
-    DownloadFile("css_white.css", fversion);
-    DownloadFile("devinfo.html", fversion);
-    DownloadFile("error.html", fversion);
-    DownloadFile("favicon.ico", fversion);
-    DownloadFile("firmware.html", fversion);
-    DownloadFile("gauge.min.js", fversion);
-    DownloadFile("header.html", fversion);
-    DownloadFile("app.js", fversion);
-    DownloadFile("lora.html", fversion);
-    DownloadFile("index.html", fversion);
-    DownloadFile("md5.js", fversion);
-    DownloadFile("md5.min.js", fversion);
-    DownloadFile("md5.min.js.map", fversion);
-    DownloadFile("password.html", fversion);
-    DownloadFile("restart.html", fversion);
-    DownloadFile("sensorv.html", fversion);
-    DownloadFile("settings.html", fversion);
-    DownloadFile("settings.js", fversion);
+  DownloadFile("css_black.css", fversion);
+  DownloadFile("css_red.css", fversion);
+  DownloadFile("css_white.css", fversion);
+  DownloadFile("devinfo.html", fversion);
+  DownloadFile("error.html", fversion);
+  DownloadFile("favicon.ico", fversion);
+  DownloadFile("firmware.html", fversion);
+  DownloadFile("gauge.min.js", fversion);
+  DownloadFile("header.html", fversion);
+  DownloadFile("app.js", fversion);
+  DownloadFile("lora.html", fversion);
+  DownloadFile("index.html", fversion);
+  DownloadFile("md5.js", fversion);
+  DownloadFile("md5.min.js", fversion);
+  DownloadFile("md5.min.js.map", fversion);
+  DownloadFile("password.html", fversion);
+  DownloadFile("restart.html", fversion);
+  DownloadFile("sensorv.html", fversion);
+  DownloadFile("settings.html", fversion);
+  DownloadFile("settings.js", fversion);
 }
 
 void DownloadFile(const char *fileName, char *fversion)
 {
-    WiFiClient client2;
-    File root2;
-    bool opened2 = false;
-    size_t fileSize = 0;
+  WiFiClient client2;
+  File targetFile;
+  bool opened2 = false;
+  size_t fileSize = 0;
 
-    const uint16_t port = 80;
-    const char *host = actconf.firmwareUpdateUrl;
+  const uint16_t port = 80;
+  const char *host = actconf.firmwareUpdateUrl;
 
-    DebugPrint(3, "Connecting to website: ");
-    DebugPrintln(3, String(host));
+  DebugPrint(3, "Connecting to website: ");
+  DebugPrintln(3, String(host));
 
-    String line = "";
-    String header = "";
-    if (client2.connect(host, port))
-    {
-        //githubpath = "data/app.js"
-        //https://raw.githubusercontent.com/bytecrusher/LoRa-Boat-Monitor/main/data/lora.html
-        
-        String url = "/files_for_esp_webserver/" + (String)fversion + "/" + (String)fileName;
-        client2.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "User-Agent: ESP32\r\n" + "Connection: close\r\n\r\n");
-//vTaskDelay(10);
-        for (int i = 0; i <= 10; i++)
-        {
-            String headertemp = client2.readStringUntil('\n');
-            header += headertemp + "\n";
-            // TODO catch Error 404 from webserver
-            if (header == "\r")
-            {
-                break;
-            }
+  if (client2.connect(host, port)) {
+    HTTPClient http;
+
+    Serial.print("[HTTP] begin...\n");
+    // configure traged server and url
+    //http.begin("https://www.howsmyssl.com/a/check", ca); //HTTPS
+    //http.begin("http://example.com/index.html"); //HTTP
+    String myurl = "https://" + (String)host + "/files_for_esp_webserver/" + (String)fversion + "/" + (String)fileName;
+    DebugPrintln(3, myurl);
+    http.begin(myurl);
+
+    Serial.print("[HTTP] GET...\n");
+    int httpCode = http.GET();
+
+    // httpCode will be negative on error
+    if(httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+      // file found at server
+      if(httpCode == HTTP_CODE_OK) {
+        String payload = http.getString();
+        //Serial.println(payload);
+        if (opened2 == false) {
+          opened2 = true;
+          targetFile = LittleFS.open((String("/") + String(fileName)).c_str(), FILE_WRITE);
+          if (!targetFile) {
+            DebugPrintln(1, "- failed to open file for writing");
+            return;
+          }
         }
-//        vTaskDelay(10);
-        line = client2.readString();
-        //  ToDo: Check if header contains not 404 !!!
-        DebugPrint(3, "data länge: ");
-        DebugPrintln(3, String(line.length()));
-        DebugPrintln(3, "The file is downloaded.");
-        //DebugPrint(3, "fileSize: ");
-        //DebugPrintln(3, String(fileSize));
-        client2.stop();
+        targetFile.print(payload);
+      } else if (httpCode == HTTP_CODE_NOT_FOUND) {
+        DebugPrintln(1, "- 404 page not found");
+      }
+    } else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
 
-        if (opened2 == false)
-        {
-            opened2 = true;
-            root2 = LittleFS.open((String("/") + String(fileName)).c_str(), FILE_WRITE);
-            if (!root2)
-            {
-                DebugPrintln(1, "- failed to open file for writing");
-                return;
-            }
-        }
-        root2.print(line);
-        /*if(root2.write(line, linelength) != linelength){
-          DebugPrintln(1, "- failed to write");
-          return;
-        }*/
-        root2.close();
-    }
-    else
-    {
-        DebugPrintln(3, "Connection unsucessful");
-    }
-    DebugPrintln(3, "end.");
+    http.end();
+    targetFile.close();
+  }
+  else
+  {
+    DebugPrintln(3, "Connection unsucessful");
+  }
+  DebugPrintln(3, "end.");
 }
 
 void sendToMDS(configData actconf)
 {
-    WiFiClientSecure client3;
-    #define protocollversion "1"
-    String MacAddress = WiFi.macAddress();
-    StaticJsonDocument<600> allSensors;
+  WiFiClientSecure client3;
+  #define protocollversion "1"
+  String MacAddress = WiFi.macAddress();
+  StaticJsonDocument<600> allSensors;
 
-    /*HTTPClient http;
-      // Your Domain name with URL path or IP address with path
-      http.begin(actconf.MdsUrl);
-      // Specify content-type header
-      http.addHeader("Content-Type", "application/json");*/
+  /*HTTPClient http;
+  // Your Domain name with URL path or IP address with path
+  http.begin(actconf.MdsUrl);
+  // Specify content-type header
+  http.addHeader("Content-Type", "application/json");*/
 
-      // Loop through each device, print out temperature data
-        //for (uint8_t i = 0; i < numberOfDevices; i++)
-        //{
-            //allSensors[i] = collectJsonData();
-        //}
+  // Loop through each device, print out temperature data
+  //for (uint8_t i = 0; i < numberOfDevices; i++)
+    //{
+      //allSensors[i] = collectJsonData();
+    //}
 
-        //allSensors[0] = collectJsonData();
+    //allSensors[0] = collectJsonData();
 
-        StaticJsonDocument<200> sensorData;
-        String buffer = "srgrdfg";
-        String tempC = "20";
-        String buffDate = "28.07.2023";
-        String buffTime = "12.55";
-        //sensorData["typid"] = 1;
-        sensorData["sensorId"] = 39;
-        //sensorData["sensorAddress"] = buffer;
-        sensorData["sensorAddress"] = 1;
-        sensorData["value1"] = String(tempC);
-        sensorData["date"] = String(buffDate);
-        sensorData["time"] = String(buffTime);
+    StaticJsonDocument<200> sensorData;
+    String buffer = "srgrdfg";
+    String tempC = "20";
+    String buffDate = "28.07.2023";
+    String buffTime = "12.55";
+    //sensorData["typid"] = 1;
+    sensorData["sensorId"] = 39;
+    //sensorData["sensorAddress"] = buffer;
+    sensorData["sensorAddress"] = 1;
+    sensorData["value1"] = String(tempC);
+    sensorData["date"] = String(buffDate);
+    sensorData["time"] = String(buffTime);
 
-        allSensors[0] = sensorData;
-        allSensors[1] = sensorData;
+    allSensors[0] = sensorData;
+    allSensors[1] = sensorData;
 
-        StaticJsonDocument<200> board;
-      StaticJsonDocument<1024> docpayload;
-      // Add values in the document
-      board["api_key"] = actconf.MdsApiKey;
-      board["protocollversion"] = protocollversion;
-      board["macaddress"] = MacAddress;
-      docpayload["board"] = board;
-      docpayload["sensors"] = allSensors;
-        
-      String requestBody;
-      serializeJson(docpayload, requestBody);
+    StaticJsonDocument<200> board;
+    StaticJsonDocument<1024> docpayload;
+    // Add values in the document
+    board["api_key"] = actconf.MdsApiKey;
+    board["protocollversion"] = protocollversion;
+    board["macaddress"] = MacAddress;
+    docpayload["board"] = board;
+    docpayload["sensors"] = allSensors;
+
+    String requestBody;
+    serializeJson(docpayload, requestBody);
 
     //DebugPrint(3, "requestBody: ");
     //DebugPrintln(3, String(requestBody));
-        
-      /*int httpResponseCode = http.POST(requestBody);
-      DebugPrint(3, "httppost via json: ");
-      DebugPrintln(3, String(requestBody));
-      DebugPrint(3, "httpResponseCode: ");
-      DebugPrintln(3, String(httpResponseCode));
-      if(httpResponseCode > 0){
-        String response = http.getString();
-        DebugPrintln(3, String(httpResponseCode));
-        DebugPrintln(3, String(response));
-      } else {
-        DebugPrintln(3, "Error code: ");
-        DebugPrintln(3, String(httpResponseCode));
-      }
 
-      // Free resources
-      http.end();*/
+    /*int httpResponseCode = http.POST(requestBody);
+    DebugPrint(3, "httppost via json: ");
+    DebugPrintln(3, String(requestBody));
+    DebugPrint(3, "httpResponseCode: ");
+    DebugPrintln(3, String(httpResponseCode));
+    if(httpResponseCode > 0){
+      String response = http.getString();
+      DebugPrintln(3, String(httpResponseCode));
+      DebugPrintln(3, String(response));
+    } else {
+      DebugPrintln(3, "Error code: ");
+      DebugPrintln(3, String(httpResponseCode));
+    }
+
+    // Free resources
+    http.end();*/
 
     client3.setInsecure();
     //client3.connect(actconf.MdsUrl, 443);
 
     DebugPrintln(3, "Starting connection to server...");
-  //if (!client3.connect(actconf.MdsUrl, 443))
-  if (!client3.connect("mds-git.derguntmar.de", 443))
-    DebugPrintln(1, "Connection failed!");
-  else {
+    //if (!client3.connect(actconf.MdsUrl, 443))
+    if (!client3.connect("mds-git.derguntmar.de", 443))
+      DebugPrintln(1, "Connection failed!");
+    else {
     DebugPrintln(3, "Connected to server!");
     // Make a HTTP request:
     //client3.println("GET https://mds-git.derguntmar.de/receiver/receivejson.php HTTP/1.0");
